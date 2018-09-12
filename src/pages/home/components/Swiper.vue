059a0b53c1299d1a464544e9d93d901fc3234992
masteralva <32874452+masteralva@users.noreply.github.com>
2018-09-12 21:52:03 +0800

    swiper

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,43 @@
+<template>
+  <swiper class="wrapper" :options="swiperOption">
+    <!-- slides -->
+    <swiper-slide>
+      <img class="swiper-img" :src="logo1" alt="">
+    </swiper-slide>
+    <swiper-slide>
+      <img class="swiper-img" :src="logo2" alt="">
+    </swiper-slide>
+    <!-- Optional controls -->
+    <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      logo1: require('/Travel/static/img/swiper1.jpg'),
+      logo2: require('/Travel/static/img/swiper2.jpg'),
+      swiperOption: {
+        pagination: '.swiper-pagination'
+      }
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper
+    width 100%
+    height 0;
+    overflow hidden
+    padding-bottom 4rem
+    background #eee
+  .wrapper >>> .swiper-pagination-bullet-active
+    background #fff
+  .swiper-img
+    width 100%
+    height 4rem
+
+</style>
